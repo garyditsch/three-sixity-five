398b3e6fcdd93d3d2796f41a5d253b6d0059f4d8
Gary Ditsch <garyditsch@gmail.com>
2024-01-09 22:12:24 -0500

    updated the behavior list styling and order

--- app/routes/list.$user.$year.tsx
+++ app/routes/list.$user.$year.tsx
@@ -30,20 +30,25 @@ export default function YearlyList() {
   console.log('ERROR', error)
   console.log('DATA', data)
 
-  const sortedList = data ? data.sort((a, b) => a.id - b.id) : [];
+  const sortedList = data ? data.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()) : [];
+
+  data?.map((day: any) => {
+    console.log(new Date(day.created_at).toString().split(' ').slice(0, 4).join(' '))
+  })
 
   return (
     <main className="max-w-full h-full flex relative overflow-y-hidden">
       {/* <!-- Container --> */}
       <div className="h-100 w-full m-4 flex flex-wrap items-start justify-start rounded-tl grid-flow-col auto-cols-auto gap-4 overflow-y-scroll">
         {/* <!-- Container --> */}
-        <div className="w-full h-100 rounded-lg grid grid-cols-1 justify-items-start gap-4"> 
+        <div className="w-full h-100 rounded-lg grid grid-cols-1 divide-y divide-slate-200 justify-items-start"> 
           {sortedList.map((day: any) => {
-            return <Link to={`/daily/2024/${day.number}`} key={day.number}>
-            <div className={"grid"}>
-              {new Date(day.created_at).toISOString().split('T')[0].split('-').slice(1).join('-')}, {day.goals.goal}
+            return <div className={"w-full grid grid-cols-1 py-4"} key={day.id}>
+              <div className="text-sm text-gray-800 font-semibold">
+                {new Date(day.created_at).toString().split(' ').slice(1, 4).join(' ')}
+              </div>
+              <div className="text-md text-gray-800">{day.goals.goal}</div>
             </div>
-            </Link>
           })}
         </div>
     </div>
