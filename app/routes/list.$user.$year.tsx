3f77a1a5aa8c737ab09f8d85bae57320a9810574
Gary Ditsch <garyditsch@gmail.com>
2024-01-12 21:30:18 -0500

    first step to getting filtering for the yearly list

--- app/routes/list.$user.$year.tsx
+++ app/routes/list.$user.$year.tsx
@@ -1,6 +1,8 @@
+import { json } from "@remix-run/node";
 import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData, useParams, Link } from "@remix-run/react";
+import { useLoaderData, Form, useParams, Link } from "@remix-run/react";
 import { createSupabaseServerClient } from "~/utils/supabase.server";
+import { useEffect } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,31 +12,50 @@ export const meta: MetaFunction = () => {
 };
 
 export async function loader({request, params}: LoaderFunctionArgs) {
+  const url = new URL(request.url);
   const { supabase } = await createSupabaseServerClient({request})
-  const { data, error } = await supabase
+  const q = url.searchParams.get("q");
+  console.log('Q', q)
+  let query = supabase
     .from('behaviors')
     .select(`
       id, goal_id, created_at, user_id,
-      goals (
-        id, goal, value, category 
+      goals!inner (
+        id, goal, value, category
       )
     `)
-  return { 
-    data: data,
-    error: error
+  if (q) {
+    query = query
+      .eq('goals.category', q)
   }
+
+  const { data, error } = await query;
+
+  return json({ 
+    data: data,
+    q: q,
+    error: error
+  })
 }
 
 export default function YearlyList() {
-  const { data, error } = useLoaderData<typeof loader>();
+  const { data, q, error } = useLoaderData<typeof loader>();
   console.log('ERROR', error)
   console.log('DATA', data)
+  console.log('Q', q)
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   const sortedList = data ? data.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()) : [];
 
-  data?.map((day: any) => {
-    console.log(new Date(day.created_at).toString().split(' ').slice(0, 4).join(' '))
-  })
+  // data?.map((day: any) => {
+  //   console.log(new Date(day.created_at).toString().split(' ').slice(0, 4).join(' '))
+  // })
 
   return (
     <main className="max-w-full h-full flex relative overflow-y-hidden">
@@ -42,6 +63,30 @@ export default function YearlyList() {
       <div className="h-100 w-full m-4 flex flex-wrap items-start justify-start rounded-tl grid-flow-col auto-cols-auto gap-4 overflow-y-scroll">
         {/* <!-- Container --> */}
         <div className="w-full h-100 rounded-lg grid grid-cols-1 divide-y divide-slate-200 justify-items-start"> 
+          <div>Fitness | Spiritual</div>
+          <div>Filtering</div>
+          <div className="py-8">
+              {/* <Form id="Fitness">
+                  <input 
+                    defaultValue={q || ""}
+                    type="hidden" 
+                    name="q" 
+                    value="Fitness"
+                    id="q"
+                  />
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Fitness</button>
+              </Form> */}
+              <Form id="search-form" role="search">
+                <input
+                  aria-label="Search contacts"
+                  defaultValue={q || ""}
+                  id="q"
+                  name="q"
+                  placeholder="Search"
+                  type="search"
+                />
+              </Form>
+          </div>
           {sortedList.map((day: any) => {
             return <div className={"w-full grid grid-cols-1 py-4"} key={day.id}>
               <div className="text-sm text-gray-800 font-semibold">
